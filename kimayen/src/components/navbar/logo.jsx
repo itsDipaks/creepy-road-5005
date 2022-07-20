7d9a7c69f1972a5a48f33feb1217a61bcebe1dbe
DIPAK PAWAR <96649241+itsDipaks@users.noreply.github.com>
2022-07-20 23:17:23 +0530

    navbar

---
+++ kimayen/src/components/navbar/logo.jsx
@@ -0,0 +1,8 @@
+function Logo(){
+    return(
+        <div>
+            <img src="https://cdn.shopify.com/s/files/1/0449/5225/6667/files/website-logo_400x.png?v=1596288204" alt="" />
+        </div>
+    )
+}
+export default Logo;
